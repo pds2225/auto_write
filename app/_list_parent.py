cb1df263858ab04a624b2f89a6b48e25aaf84cb2
pds2225 <ekth3691@gmail.com>
2026-07-27 14:12:31 +0900

    auto-snapshot 2026-07-27 13:43:52 (dirty=6)

--- app/_list_parent.py
+++
@@ -1,7 +0,0 @@
-import os
-p = r"C:\Users\ekth3\OneDrive\바탕 화면\지원사업_공고첨부_문서전용_20260625"
-if os.path.isdir(p):
-    for name in os.listdir(p):
-        print(name)
-else:
-    print("PARENT NOT FOUND")
